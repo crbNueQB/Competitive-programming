4bc8ca61fa240732ee907b6e2c4afa4273baa54b
bantasica <kihara.akio@gmail.com>
2021-03-31 02:27:18 +0000

    sample

--- template/random.cpp
+++ template/random.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 #define WIN true
 #define LOSE false
-#define COST 0.6
-#define SPREAD 0.2
-#define PROFIT 1.0
-#define DEPROFIT 1.0
+#define COST 0
+#define SPREAD 1.2
+#define PROFIT 4.2
+#define DEPROFIT 2.0
+#define bLot 0.01
+#define tpf 4.75
 
 list<int> monte;
 
@@ -16,11 +18,11 @@ int return_random(){
     bool w;
     std::random_device rnd;     // 非決定的な乱数生成器を生成
     std::mt19937 mt(rnd());     //  メルセンヌ・ツイスタの32ビット版、引数は初期シード値
-    std::uniform_int_distribution<> rand100(0, 999);        // [0, 999] 範囲の一様乱数
+    std::uniform_int_distribution<> rand100(1, 1000);        // [0, 999] 範囲の一様乱数
 
     s = rand100(mt);
     
-    if(s <= 505) w = WIN;
+    if(s <= 510) w = WIN;
     else w = LOSE;
     return w;
 }
@@ -33,11 +35,9 @@ void init_monte(){
 
 void calc_monte(bool w){
     if(w){
-        auto itr = monte.begin();
-        monte.erase(itr);
+        monte.pop_front();
         if(!monte.empty()){            
-            auto itrr = monte.end();
-            monte.erase(itrr);
+            monte.pop_back();
         }
     }else{
             monte.push_back(monte.front() + monte.back());
@@ -48,26 +48,30 @@ int main() {
     //cin >> ;
     init_monte();
     int Lots = 0;
+    double sumpf = 0;
     double pf = 0;
-    for (int i = 1; i <= 1000; ++i){
+    for (int i = 1; i <= 100000; ++i){
         if(monte.empty()) init_monte();
         Lots = monte.front() + monte.back();
         if(return_random()){
             pf += (PROFIT - COST - SPREAD) * Lots;
+            sumpf += Lots * bLot * tpf;
             calc_monte(WIN);
         }else{
             pf -= (DEPROFIT + COST + SPREAD) * Lots;
+            sumpf += Lots * bLot * tpf;
             calc_monte(LOSE);
         }
-        
+        /*
         if(!monte.empty()){
             for (auto lst = monte.begin(); lst != monte.end(); ++lst){
                 cout << *lst << " ";
             }
             cout << endl;
-        }
-        cout << "pf" << pf << endl;
+        }*/
+        //cout << "pf" << pf << endl;
     }
     cout << "spf" << pf << endl;
+    cout << "tpf" << sumpf << endl;
     return 0;
 }
