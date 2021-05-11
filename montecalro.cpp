1a80fab246e7b221cddf3f676b77e72169552484
crbNueQB <kihara.akio+github@gmail.com>
2021-05-11 23:58:26 +0000

    update

--- montecalro.cpp
+++ montecalro.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define SPREAD 1.2
 #define PROFIT 4.2
 #define DEPROFIT 2.0
-#define bLot 0.01
+#define bLot 0.02
 #define tpf 4.75
 
 list<int> monte;
@@ -18,7 +18,7 @@ int return_random(){
     bool w;
     std::random_device rnd;     // 非決定的な乱数生成器を生成
     std::mt19937 mt(rnd());     //  メルセンヌ・ツイスタの32ビット版、引数は初期シード値
-    std::uniform_int_distribution<> rand100(1, 1000);        // [0, 999] 範囲の一様乱数
+    std::uniform_int_distribution<> rand100(1, 1000);        // [1, 1000] 範囲の一様乱数
 
     s = rand100(mt);
     
@@ -49,31 +49,39 @@ int main() {
     init_monte();
     int Lots = 0;
     int mlot = 0;
+    int losscut = 0;
     double sumpf = 0;
-    double pf = 100000;
+    double pf = 20000;
+    double mpf = pf;
+    double bonus = 0;
     for (int i = 1; i <= 100000; ++i){
         if(monte.empty()) init_monte();
         Lots = monte.front() + monte.back();
-        if(Lots >= 40) {
+        if(Lots >= 0) {
             init_monte();
             Lots = monte.front() + monte.back();
+            losscut++;
         }
         mlot = max(Lots, mlot);
         if(return_random()){
-            pf += 50 * Lots * 0.01 - 7 * Lots * 0.01;
+            pf += 6000 * Lots * bLot - 700 * Lots * bLot ;
             //sumpf += Lots * bLot * tpf;
             calc_monte(WIN);
         }else{
-            pf -= 50 * Lots * 0.01- 7 * Lots * 0.01;
+            pf -= 6000 * Lots * bLot - 700 * Lots * bLot ;
             //sumpf += Lots * bLot * tpf;
             calc_monte(LOSE);
         }
-        if(pf <= 0) break;
-
+        if(pf <= 216) break;
+        mpf = min(mpf, pf);
+        //bonus += 150 * bLot * Lots;
+        bonus += 3;
         //cout << "pf" << pf << endl;
     }
     cout << "mlot" << mlot << endl;
     cout << "spf" << pf << endl;
-    //cout << "tpf" << sumpf << endl;
+    cout << "mpf" << mpf << endl;
+    cout << "losscut" << losscut << endl;
+    cout << "bonus" << bonus << endl;
     return 0;
 }
